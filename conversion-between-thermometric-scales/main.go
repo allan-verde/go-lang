ad1cf4384115abe86d4496e087aa066d85f89d68
Allan Verde <allangreen2906@gmail.com>
2023-05-30 09:52:50 -0300

    feat: desafio conversão entre escalas termométricas

--- conversion-between-thermometric-scales/main.go
+++ conversion-between-thermometric-scales/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+const tempKelvin = 373
+
 func main() {
-	fmt.Printf("")
+	tempCelcius := tempKelvin - 273
+	fmt.Printf("A temperatura de ebulição da água em ºK = %v, e a temperatura de ebulição da água em ºC = %v.", tempKelvin, tempCelcius)
 }
